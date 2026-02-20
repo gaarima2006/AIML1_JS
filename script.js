21688cede19d433fb36077e45ffae9c98869f7df
gaarima2006 <garimagupta222006@gmail.com>
2026-02-20 10:31:19 +0530

    java

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+// console.log("Hello World");
+// document.write("Hello World");
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num="garima";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+//function
+// var sayhello= function()
+// {
+//     alert("hello world");
+// }
+// //array'
+// var numberArray=[1,2,3];
+
+
+
+// const symbl=Symbol(4);
+// console.log(symbl);
+// const sym2=Symbol(4);
+// console.log(sym2);
+// console.log("true");
+var a=100,var b=13,var c=10,var linebreak="<br>";
+document.write("a+b+c=");
+result=a=b=c;
+document.write("result");
+document.write("linebreak");
