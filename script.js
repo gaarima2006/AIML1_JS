0d18290a7ada20fa9e7f3cd4771d5f2a132d0166
gaarima2006 <garimagupta222006@gmail.com>
2026-02-23 15:42:52 +0530

    operators and loop

--- script.js
+++ script.js
@@ -1,64 +1,148 @@
 // console.log("Hello World");
 // document.write("Hello World");
+
 // var num = 10;
 // console.log(num);
 // console.log(typeof(num));
 // num = false;
 // console.log(num);
 // console.log(typeof(num));
-// num="garima";
+// num = "Geet Tikkha";
 // console.log(num);
 // console.log(typeof(num));
-// num=null;
+// num = null;
 // console.log(num);
 // console.log(typeof(num));
-// num=undefined;
-// console.log(num);
+// num = undefined;
 // console.log(typeof(num));
-//function
-// var sayhello= function()
-// {
+
+// // function
+// var sayHello = function() {
 //     alert("hello world");
 // }
-// //array'
-// var numberArray=[1,2,3];
 
+// // array
+// var numberArray = [1, 2, 3];
+// var animals = new Array("cat", "dog", "mouse", "lion");
 
-
-// const symbl=Symbol(4);
-// console.log(symbl);
-// const sym2=Symbol(4);
-// console.log(sym2);
-// console.log("true");
-// var a=100,var b=13,var c=10,var linebreak="<br>";
-// document.write("a+b+c=");
-// result=a=b=c;
-// document.write("result");
-// document.write("linebreak");
-// var a= 10;
-// var b=20;
-// var linebreak="<br/>>";
-// result=(a==b);
-// document.write("result");
-// document.write("linebreak");
-// document.write("(a<b)=>");
-// result=(a<b);
-// document.write("result");
-//  document.write("linebreak");
-//  document.write("(a>b)=>");
-//  document.write("linebreak");
-// var count;
-// document.write("starting loop"+"<br/>");
-// for(count=0;count<10;count++)
-// {
-// document.write("current count" + count);
-// document.write("<br/>");
+// // object
+// var person = {
+//     name: 'Barack Hussein Obama II',
+//     age: '51',
+//     title: '44th President of the United States'
 // }
-const person={
-    fname:"ajay",
-    lname:"sngh",
-    age:25
-};
-for(let x in person){
-    console.log("person details:"+x+":"+person[x])
+
+
+
+
+// const sym1 = Symbol(4);
+// console.log(sym1);
+// const sym2 = Symbol(4);
+// console.log(sym2);
+// if (sym1 == sym2) {
+//     console.log("true");
+// }
+// else
+// {
+//     console.log("false")  // because each symbol is unique
+// }
+
+// {
+//     alert("test");
+
+
+// }
+
+// var a = 100; var b = 13; var c = 10; var linebreak = "<br>";
+
+// document.write("a + b + c =");
+// result = a + b + c;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a + b =");
+// result = a + b;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a - b=");
+// result = a - b;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a / b=");
+// result = a / b;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a % b=");
+// result = a % b;
+// document.write(result);
+// document.write(linebreak);
+
+
+
+// var a = 10;
+// var b = 20;
+// var linebreak = "<br />";
+
+// document.write ("(a == b) =>");
+// result = (a == b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write ("(a < b) =>");
+// result = (a < b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write ("(a > b) =>");
+// result = (a > b);
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write ("(a != b) =>");
+// result = (a != b);
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write ("(a >= b) =>");
+// result = (a >= b);
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write ("(a <= b) =>");
+// result = (a <= b);
+// document.write(result);
+// document.write(linebreak);
+
+
+
+
+
+// loops
+
+// var count;
+// document.write("starting loop" + "<br />");
+// for (count = 0; count < 10; count++) {
+//     document.write("current count:" + count);
+//     document.write("<br />");
+// }
+// document.write("loop stopped!");
+
+
+// for in loop
+// for(key in object) {
+// code block to be executed
+// }
+const person = {
+fname: "Ajay",
+Iname: "Singh",
+age: 25
+}
+for (let x in person) {
+console.log("person details: " + x + ":" + person[x])
 }
