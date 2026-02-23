7e8a9571e894f5cbe0ab0a78a1fd1e9f2a58f73c
gaarima2006 <garimagupta222006@gmail.com>
2026-02-23 15:29:27 +0530

    operators

--- script.js
+++ script.js
@@ -30,8 +30,30 @@
 // const sym2=Symbol(4);
 // console.log(sym2);
 // console.log("true");
-var a=100,var b=13,var c=10,var linebreak="<br>";
-document.write("a+b+c=");
-result=a=b=c;
-document.write("result");
-document.write("linebreak");
+// var a=100,var b=13,var c=10,var linebreak="<br>";
+// document.write("a+b+c=");
+// result=a=b=c;
+// document.write("result");
+// document.write("linebreak");
+// var a= 10;
+// var b=20;
+// var linebreak="<br/>>";
+// result=(a==b);
+// document.write("result");
+// document.write("linebreak");
+// document.write("(a<b)=>");
+// result=(a<b);
+// document.write("result");
+//  document.write("linebreak");
+//  document.write("(a>b)=>");
+//  document.write("linebreak");
+var count;
+document.write("starting loop"+"<br/>");
+for(count=0;count<10;count++)
+{
+document.write("current count" + count);
+document.write("<br/>");
+}
+const person={
+    fname:"ajay"
+}
