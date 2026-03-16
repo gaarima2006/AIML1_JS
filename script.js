e52bb5787a04e3362833c95bd0abcab669974779
gaarima2006 <garimagupta222006@gmail.com>
2026-03-16 15:39:56 +0530

    donk

--- script.js
+++ script.js
@@ -227,5 +227,23 @@
 // const result=document.getElementById("para");
 // result.innerHTML="my self";
 // result.title="myname";
-const result2=document.getElementsByClassNae("heading");
-result2.innerHTNL="hii";
+// const result2=document.getElementsByClassNae("heading");
+// result2.innerHTNL="hii";
+// document.getElementsByTagName("p")
+// result3[2].innerHTML ="this is a pet";
+// result3[1].style.color ="blue";
+// result3[1].innerText ="my paragraph";
+// const element = document.getElementById("myh1");
+// let text=element.getAttribute("class");
+// document.getElementById("demo").innerHTML=text;
+// function myFunction()
+// {
+    // document.getElementById("myh1").setAttribute("class","democlass");
+// // }
+// const heading =document.createElement("h2");
+// const bodytag =document.getElementsByTagName("body");
+// bodytag[0],append("heading");
+function onhandlfocus(){
+    x.style.background="red";
+
+}
