fc3f661e6bb14de655f5c91f2e75c3d0a6b8dc00
gaarima2006 <garimagupta222006@gmail.com>
2026-04-10 23:04:45 +0530

    donk

---
+++ donk.js
@@ -0,0 +1,43 @@
+console.log(collection);
+const result=document.getElementById("para");
+result.innerHTML="my self";
+result.title="myname";
+const result2=document.getElementsByClassNae("heading");
+result2.innerHTNL="hii";
+document.getElementsByTagName("p")
+result3[2].innerHTML ="this is a pet";
+result3[1].style.color ="blue";
+result3[1].innerText ="my paragraph";
+const element = document.getElementById("myh1");
+let text=element.getAttribute("class");
+document.getElementById("demo").innerHTML=text;
+function myFunction()
+{
+    document.getElementById("myh1").setAttribute("class","democlass");
+// }
+const heading =document.createElement("h2");
+const bodytag =document.getElementsByTagName("body");
+bodytag[0],append("heading");
+function onhandlfocus()
+
+{      x.style.background="red";
+    <input type="text" onkeydown="customizeInput()"></input>}
+    
+function customizeInput() {
+  var ele = document.getElementsByTagName("INPUT")[0];
+  ele.style.backgroundColor = "yellow";
+  ele.style.color = "red";
+}
+
+const heading = document.createElement("h2");
+const bodytag = document.getElementsByTagName("body");
+bodytag[0].append(heading);
+
+heading.innerText = "hello";
+heading.style.backgroundColor = "red";
+const heading = document.createElement("h2");
+const b= document.getElementsByTagName("body");
+bodytag[0].append(heading);
+
+heading.innerText = "hello";
+heading.style.backgroundColor = "red";
